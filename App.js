98234c470d427ebb214f99e86cdb141bb499f0f2
오명훈 <potahog@omyeonghun-ui-MacBookPro.local>
2020-01-06 01:29:04 +0900

    #4 Coding the UI part Tow Input and To Do Component

--- App.js
+++ App.js
@@ -6,21 +6,45 @@ import {
   StatusBar, 
   TextInput, 
   Dimensions,
-  Platform 
+  Platform, 
+  ScrollView
 } from 'react-native';
+import Todo from "./ToDo";
 
 const {height, width} = Dimensions.get("window");
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" />
-      <Text style={styles.title}>Kawai To Do</Text>
-      <View style={styles.card}>
-        <TextInput style={styles.input} placeholder={"New To Do"} />
+export default class App extends React.Component {
+  state ={
+    newToDo: ""
+  };
+  render(){
+    const { newToDo } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Kawai To Do</Text>
+        <View style={styles.card}>
+          <TextInput 
+          style={styles.input} 
+          placeholder={"New To Do"} 
+          value={newToDo} 
+          onChange={this._contorllNewTodo}
+          placeholderTextColor={"#999"}
+          returnKeyType={"done"}
+          autoCorrect={false}
+          />
+          <ScrollView>
+              <Todo />
+          </ScrollView>
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
+  _contorllNewTodo = text =>{
+    this.setState({
+      newTodo: text
+    })
+  }
 }
 
 const styles = StyleSheet.create({
@@ -56,5 +80,11 @@ const styles = StyleSheet.create({
         elevation: 3,
       }
     })
+  },
+  input:{
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 25,
   }
 });
---
+++ ToDo.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import {View, Text, TouchbleOpacity, StyleSheet} from "react-native";
+
+export default class ToDo extends Component{
+    render(){
+        return( 
+        <View>
+            <Text></Text>
+        </View>
+        );
+    }
+}
+
+const style = StyleSheet.create({
+
+});
