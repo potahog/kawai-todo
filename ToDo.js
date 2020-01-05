33339d7ba6fdd238b63e1d81c7975f1a499530e9
오명훈 <potahog@omyeonghun-ui-MacBookPro.local>
2020-01-06 02:00:28 +0900

    #5 Coding the UI part Three - To Do COmponent

--- ToDo.js
+++ ToDo.js
@@ -1,16 +1,74 @@
-import React, {Component} from "react";
-import {View, Text, TouchbleOpacity, StyleSheet} from "react-native";
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  
+} from "react-native";
 
-export default class ToDo extends Component{
-    render(){
-        return( 
-        <View>
-            <Text></Text>
-        </View>
-        );
-    }
+const { width, height } = Dimensions.get("window");
+
+export default class ToDo extends Component {
+  state = {
+    isEditing: false,
+    isCompleted: false,
+  };
+  render() {
+      const { isCompleted } = this.state;
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this._completeToDo}>
+          <View style={[
+              styles.circle, 
+              isCompleted ? styles.completedCircle : styles.unCompletedCircle
+              ]} 
+            />
+        </TouchableOpacity>
+        <Text style={styles.text}>Hello I'm a To Do</Text>
+      </View>
+    );
+  }
+
+  _completeToDo = () =>{
+        this.setState(prevState =>{
+            return {
+                isCompleted: !prevState.isCompleted
+            };
+        });
+  }
+
+  _toggleComplete = () =>{
+
+  }
 }
 
-const style = StyleSheet.create({
-
+const styles = StyleSheet.create({
+  container: {
+    width: width - 50,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderColor: "red",
+    borderWidth: 3,
+    marginRight: 20,
+  },
+  completedCircle: {
+    borderColor: "#bbb"
+  },
+  unCompletedCircle: {
+    borderColor: "#F23657"
+  },
+  text: {
+    fontWeight: "600",
+    fontSize: 20,
+    marginVertical: 20,
+  }
 });
