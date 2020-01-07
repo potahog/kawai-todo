ebba8a3f59d8b8f9603d5c477c9e9c87c1838dee
오명훈 <potahog@omyeonghun-ui-MacBookPro.local>
2020-01-07 19:52:03 +0900

    #13 Saving Changes to the Disk

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ import {
   TextInput,
   Dimensions,
   Platform,
-  ScrollView
+  ScrollView,
+  AsyncStorage
 } from "react-native";
 import { AppLoading } from "expo";
 import ToDo from "./ToDo";
@@ -26,7 +27,6 @@ export default class App extends React.Component {
   };
   render() {
     const { newToDo, loadedToDos, toDos } = this.state;
-    console.log(toDos);
     if (!loadedToDos) {
       return <AppLoading />;
     }
@@ -155,9 +155,14 @@ export default class App extends React.Component {
           }
         }
       };
+      this._saveToDos(newState.toDos);
       return { ...newState };
     });
   };
+
+  _saveToDos = (newToDos) =>{
+    const saveToDos = AsyncStorage.setItem("toDos", JSON.stringify(newToDos));
+  }
 }
 
 const styles = StyleSheet.create({
